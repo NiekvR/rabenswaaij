7ed699b72bbdd93a4191d3555fe19ec6e88fb72c
Niek van Rabenswaaij <>
2018-10-14 21:02:04 +0200

    Finishing touch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,7 +16,14 @@ export class HomeComponent implements OnInit, AfterViewInit {
   public detailProject: Project;
   public detailBlockButtonHidden: boolean = false;
   public selectedProject: string;
-  public displayArrow: boolean;
+  public displayApp: boolean;
+  private detailBlockPositionPerListElement: Array<number>;
+  private detailBlockPositionInList: Array<number>;
+  private listItemSizeMap = {
+    'sm': 200,
+    'md': 300,
+    'lg': 400
+  };
 
   constructor(private el: ElementRef, private renderer: Renderer2, private dataService: DataService) { }
 
@@ -27,19 +34,45 @@ export class HomeComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     this.detailBlockElement = this.el.nativeElement.querySelector('.item.detail');
     this.projectListElement = this.el.nativeElement.querySelector('ul.project-list');
-    this.arrowEl = this.el.nativeElement.querySelector('.arrow-text');
-    console.log(this.arrowEl);
   }
 
   getProjects(): void {
     this.dataService.getTERNProjects()
         .subscribe(projects => {
           this.projects = projects;
-          console.log(projects);
+          console.log(projects.length);
+          this.mapListElements();
         });
   }
 
-  detailBlock(index: number, project: Project) {
+  mapListElements() {
+    this.detailBlockPositionPerListElement = new Array<number>();
+    this.detailBlockPositionInList = new Array<number>();
+    let currentRow = 0;
+    let currentRowPixel = 300;
+    this.detailBlockPositionPerListElement.push(currentRow);
+    if(window.screen.width > 740) { 
+      let viewPortWidth = window.screen.width < 1000 ? window.screen.width : 1000;
+      this.projects.forEach((project, index) => {
+        currentRowPixel += this.listItemSizeMap[project.size];
+        if(currentRowPixel > viewPortWidth) {
+          this.detailBlockPositionInList.push(index + 1);
+          currentRowPixel = this.listItemSizeMap[project.size];
+          currentRow++
+        };
+        this.detailBlockPositionPerListElement.push(currentRow);
+      });
+      this.detailBlockPositionInList.push(this.projects.length + 1);
+    } else {
+      this.projects.forEach((project, index) => {
+        this.detailBlockPositionPerListElement.push(index);
+        this.detailBlockPositionInList.push(index + 2);
+      });
+    }
+  }
+
+  detailBlock(event, index: number, project: Project) {
+    console.log(event);
     if (!this.detailProject) {
       this.selectedProject = project.title;
       this.openDetailBlock(index, project);
@@ -54,16 +87,14 @@ export class HomeComponent implements OnInit, AfterViewInit {
     }
   }
 
-  getDetailBlockPosition(index: number): number {
-    return Math.floor((index + 1) / 3) * 3 + 3;
-  }
-
   openDetailBlock(index: number, project: Project) {
     this.renderer.removeChild(this.projectListElement, this.detailBlockElement);
-    this.renderer.insertBefore(this.projectListElement, this.detailBlockElement, this.projectListElement.querySelectorAll('li')[this.getDetailBlockPosition(index)]);
+    this.renderer.insertBefore(this.projectListElement, this.detailBlockElement, this.projectListElement.querySelectorAll('li')[this.detailBlockPositionInList[this.detailBlockPositionPerListElement[index + 1]]]);
     this.detailProject = project;
     setTimeout(() => {
-      if(project.title === 'Veldweekend') {
+      if(window.screen.width < 740) {
+        this.renderer.setStyle(this.detailBlockElement, 'height', '400px');
+      } else if (project.title === 'Veldweekend') {
         this.renderer.setStyle(this.detailBlockElement, 'height', '500px');
       } else {
         this.renderer.setStyle(this.detailBlockElement, 'height', '300px');
@@ -71,14 +102,15 @@ export class HomeComponent implements OnInit, AfterViewInit {
     }, 25);
     setTimeout(() => {
       if(project.title === 'Veldweekend') {
-        this.displayArrow = true;
+        this.displayApp = true;
       }
     }, 1500)
     this.detailBlockButtonHidden = true;
   }
 
   closeDetailBlock() {
-    this.displayArrow = false;
+    this.selectedProject = null;
+    this.displayApp = false;
     setTimeout(() => {
       this.detailProject = null;
     }, 700);
--- src/assets/data/tern-projects.ts
+++ src/assets/data/tern-projects.ts
@@ -26,10 +26,12 @@ export const PROJECTS: Project[] = [
     headerImage: 'assets/img/veldweekend.jpg',
     title: 'Veldweekend',
     excerpt: 'Een digitaal program- maboek voor een lang weekend weg',
-    text: '<p>De responsive website van Nicole bestaat uit een aantal statische pagina\'s ' + 
-    ', een simpel contact formulier wat resulteerd in een mail naar Nicole en een google maps ' +
-    'implementatie voor een routebeschrijving.</p> <p>De website is een angular 5 app, gehost ' +
-    ' in firebase en maakt gebruikt van firebase functions.</p>',
+    text: '<p>Een website gebouwd om op een mobile device te gebruiken.</p>' + 
+    '<p>In dit project vervangt de website het papiere programmaboekje van het evenement.' +
+    'Het maakt gebruik van alle mogelijkheden die dit bied. De wandelingen kunnen in detail bekeken worden' +
+    'in google maps, een overzicht van mogenlijk vogelsoorten is natuurlijk niet compleet zonder geluid en ' +
+    'de sudoku hoeft niet met de pen worden ingevuld!</p>' +
+    'Deze website is nog oldskool: html, css en javascript.',
     frameworks: {
       html: true,
       css: true,
@@ -37,7 +39,7 @@ export const PROJECTS: Project[] = [
     },
     size: 'sm',
     color: '#76B542',
-
+    detailImage: 'assets/img/website-veldweekend.png',
     url: 'http://www.ternwebdesign.nl/veldweekend/'
   },
   {
@@ -45,8 +47,12 @@ export const PROJECTS: Project[] = [
     headerImage: 'assets/img/kuiken_teller.jpeg',
     headerPosition: '-496px',
     title: 'Kuiken Teller',
-    excerpt: '',
-    text: '',
+    excerpt: 'Een website en mobiele applicatie voor een citizens-science project om zoveel mogelijk waarnemingen van eendenkuikens te vergaren.',
+    text: '<p>Om te onderzoeken wat eendenkuikensverleving hiermee te maken heeft' +
+    'is dit project bedacht. Dit is begonnen met een formulier waarmee je je eendenkuiken waarneming kon versturen: je locatie geef ' +
+    'je in via google maps en in geval van twijfel kun je zelfs een foto uploaden.</p>' +
+    '<p>Na het succes van het formulier is dit uitgebreid met een app voor Android en iOS om het de gebruiker nog makkelijker te ' +
+    'maken een waarneming in te sturen.',
     frameworks: {
       html: true,
       sass: true,
@@ -57,14 +63,18 @@ export const PROJECTS: Project[] = [
       android: true,
     },
     size: 'lg',
-    color: '#55B6D7'
+    color: '#55B6D7',
+    detailImage: 'assets/img/app-store-kuiken-teller.png',
+    url: 'http://www.erikkleyheeg.nl/eendenkuikens'
   },
   {
     stack: 'frontend',
     headerImage: 'assets/img/biologie_in_het_werkveld.jpg',
     title: 'Biologie in het Werkveld',
-    excerpt: '',
-    text: '',
+    excerpt: 'Tool voor docenten van de biologie in het werkveld cursessen om lescontent bij studenten te krijgen.',
+    text: '<p>Deze website maakt gebruik van wordpress om docenten de mogelijkheid te bieden mededelingen te ' +
+    ' versturen, presntaties te delen of extra lescontent te verschaffen.</p>' +
+    'De basis van deze website zijn een aantal html, css en javascript templates die doormiddel van php in wordpress worden geladen.</p>',
     frameworks: {
       html: true,
       css: true,
@@ -73,21 +83,26 @@ export const PROJECTS: Project[] = [
       wordpress: true
     },
     size: 'md',
-    color: '#BED63A'
+    color: '#BED63A',
+    detailImage: 'assets/img/website-biologie-in-het-werkveld.png',
+    url: 'http://biologieinhetwerkveld.org'
   },
   {
     stack: 'frontend',
     headerImage: 'assets/img/natuurfotografie.jpg',
     title: 'TERN Natuurfotografie',
-    excerpt: '',
-    text: '',
+    excerpt: 'Persoonlijk foto-archief',
+    text: '<p>Ik en mijn vrouw maken zelf graag foto\'s en dit is ons digitale archief</p>' +
+    '<p>Dit is een angularjs applicatie</p>',
     frameworks: {
       html: true,
       css: true,
       js: true,
       angular: true
     },
-    size: 'md',
-    color: '#76B542'
+    size: 'sm',
+    color: '#76B542',
+    detailImage: 'assets/img/website-nr-fotografie.png',
+    url: 'http://www.ternwebdesign.nl/#/nrnatuurfotografie/'
   }
 ];
